b174052d4602cb59d4af15ec6e6b49cedc64347f
Loic Pefferkorn <loic-git@loicp.eu>
2013-10-10 20:35:49 +0200

    Update Evolve Thinking functions extractor helper

--- tools/extract_cf3evolve_freelib.sh
+++ tools/extract_cf3evolve_freelib.sh
@@ -3,9 +3,11 @@
 # Extract functions from Evolve Thinking's free promise libary. Should be run
 # inside masterfiles:
 #
+# git clone https://github.com/evolvethinking/evolve_cfengine_freelib.git
+# cd evolve_cfengine_freelib
 # ~/path/to/tools/extract_cf3evolve_freelib.sh
 
-efl=$1 
-awk '/^(bundle|body)/ {gsub(/\(.*/, "", $3);printf " %s", $3}' $efl  |\
-sort|awk '{count=split($0, tab); asort(tab);for (i=0;i<=count;i++) { if (i % 6 ==0) printf "MARK";printf "%s ", tab[i]}} END{ print "contained"}'|\
+find . -type f -name 'evolve_freelib.cf' | xargs cat\
+| awk '/^(bundle|body)/ {gsub(/\(.*/, "", $3);printf " %s", $3}'\
+| awk '{count=split($0, tab); asort(tab);for (i=0;i<=count;i++) { if (i % 6 ==0) printf "MARK";printf "%s ", tab[i]}} END{ print "contained"}'|\
 sed 's/MARK/contained\nsyn keyword cf3Evolve_freelib\t/g'|tail -n +2
